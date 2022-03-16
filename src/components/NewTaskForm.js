f1d3685bea88a8f9ffa793e278f54c9d0c0c2973
Corey Fisher <coreyfisher2021@gmail.com>
2022-03-16 15:07:22 -0500

    commit tree b4e956f5f23387d1d7dc74b566daaf9716d77917

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+
+  const [text, setText] = useState()
+  const [category, setCategory] = useState(categories[0])
+
+  function handleDetailsChange(event) {
+    setText(event.target.value)
+  }
+
+  function handleOnChange(event) {
+    setCategory(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newTask = {text: text, category: category}
+    onTaskFormSubmit(newTask)
+  }
+
+  const optionElements = categories.map(category => {
+    return <option key={category}>{category}</option>
+  });
+
   return (
-    <form className="new-task-form">
+    <form onSubmit={handleSubmit} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input onChange={handleDetailsChange} type="text" name="text"/>
       </label>
       <label>
         Category
-        <select name="category">
+        <select onChange={handleOnChange} name="category">
           {/* render <option> elements for each category here */}
+          {optionElements}
         </select>
       </label>
-      <input type="submit" value="Add task" />
+      <input type="submit" value="Add task"/>
     </form>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({ tasks, handleTaskDeleteFunction }) {
+  const tasksArray = tasks.map((tasks, index) => {
+    return <Task
+      text={tasks.text}
+      category={tasks.category}
+      key={index}
+      onDeleteTask={handleTaskDeleteFunction}
+    />
+  })
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasksArray}
     </div>
   );
 }
